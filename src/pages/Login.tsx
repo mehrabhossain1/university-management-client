80abe9deea3a716d407ba476ade2e88bf1c6f6b3
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-09-06 19:13:38 +0600

    15-5 Basic Login form and useLoginMutaion

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,38 @@
-const login = () => {
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+
+const Login = () => {
+  const { handleSubmit, register } = useForm();
+
+  const [login, { data, error }] = useLoginMutation();
+
+  console.log(data, error);
+
+  const onSubmit = (data) => {
+    console.log("onsubmit data", data);
+
+    const userInfo = {
+      id: data.id,
+      password: data.password,
+    };
+
+    login(userInfo);
+  };
+
   return (
-    <div>
-      <h1>This is login component</h1>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">ID: </label>
+        <input type="text" id="id" {...register("id")} />
+      </div>
+      <div>
+        <label htmlFor="password">Password: </label>
+        <input type="text" id="password" {...register("password")} />
+      </div>
+      <Button htmlType="submit">Login</Button>
+    </form>
   );
 };
 
-export default login;
+export default Login;
