aeb56a544eadc91107b0689e6334e46c2d8c78b6
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-09-11 12:36:03 +0600

    18-9 Resolve type errors of Redux Response

---
+++ src/types/academicManagement.type.ts
@@ -0,0 +1,11 @@
+export type TAcademicSemester = {
+  _id?: string;
+  name: string;
+  year: string;
+  code: string;
+  startMonth: string;
+  endMonth: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+  __v?: number;
+};
