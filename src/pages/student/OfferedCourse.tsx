676cf076685e95793ee55ad16fd0cd3229bb5a7c
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-09-25 15:20:19 +0600

    21-5 Handle student offered course

--- src/pages/student/OfferedCourse.tsx
+++ src/pages/student/OfferedCourse.tsx
@@ -1,4 +1,9 @@
+import { useGetAllOfferedCoursesQuery } from "../../redux/features/student/studentCourse.api";
+
 const OfferedCourse = () => {
+  const { data } = useGetAllOfferedCoursesQuery(undefined);
+  console.log(data);
+
   return (
     <div>
       <h1>This is OfferedCourse component</h1>
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,8 @@
-export * from "./sidebar.type"; //* is used to export all the types from the sidebar.type.ts file.. It is called barrel. It is a workflow or system...
+export * from "./sidebar.type";
 export * from "./global.type";
 export * from "./academicManagement.type";
 export * from "./userManagement.type";
 export * from "./courseManagement.type";
+export * from "./studentCourse.type";
+
+//? is used to export all the types from the sidebar.type.ts file.. It is called barrel. It is a workflow or system...
