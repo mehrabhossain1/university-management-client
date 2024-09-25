6096d745ad0964e2e8206cf3c2fbe4c8dde6af92
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-09-25 19:32:15 +0600

    21-7 Student enroll course UI

--- src/pages/student/OfferedCourse.tsx
+++ src/pages/student/OfferedCourse.tsx
@@ -1,3 +1,4 @@
+import { Button, Col, Row } from "antd";
 import { useGetAllOfferedCoursesQuery } from "../../redux/features/student/studentCourse.api";
 
 const OfferedCourse = () => {
@@ -11,17 +12,51 @@ const OfferedCourse = () => {
     acc[key].sections.push({
       section: item.section,
       _id: item._id,
+      days: item.days,
+      startTime: item.startTime,
+      endTime: item.endTime,
     });
 
     return acc;
   }, {});
 
-  console.log(singleObject);
+  const modifiedData = Object.values(singleObject ? singleObject : {});
 
   return (
-    <div>
-      <h1>This is OfferedCourse component</h1>
-    </div>
+    <Row gutter={[0, 20]}>
+      {modifiedData?.map((item) => (
+        <Col
+          key={item.courseTitle}
+          span={24}
+          style={{ border: "2px solid #d4d4d4" }}
+        >
+          <div style={{ padding: "10px" }}>
+            <h1>{item.courseTitle}</h1>
+          </div>
+          <div>
+            {item.sections?.map((section) => (
+              <Row
+                key={section._id}
+                justify="space-between"
+                align="middle"
+                style={{ borderTop: "2px solid #d4d4d4", padding: "10px" }}
+              >
+                <Col span={5}>Section: {section.section}</Col>
+                <Col span={5}>
+                  Days:
+                  {section.days.map((day) => (
+                    <span> {day} </span>
+                  ))}
+                </Col>
+                <Col span={5}>Start Time: {section.startTime}</Col>
+                <Col span={5}>End Time: {section.endTime}</Col>
+                <Button>Enroll</Button>
+              </Row>
+            ))}
+          </div>
+        </Col>
+      ))}
+    </Row>
   );
 };
 
