c1bb933dc4552f85f2df12a20b95ea8fb9745092
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-09-28 19:18:53 +0600

    21-9 Handle Student schedule

---
+++ src/pages/student/MySchedule.tsx
@@ -0,0 +1,24 @@
+import { useGetAllEnrolledCoursesQuery } from "../../redux/features/student/studentCourse.api";
+
+const MySchedule = () => {
+  const { data } = useGetAllEnrolledCoursesQuery(undefined);
+  console.log(data);
+
+  return (
+    <div>
+      {data?.data?.map((item) => (
+        <div>
+          <div>{item.course.title}</div>
+          <div>{item.offeredCourse.section}</div>
+          <div>
+            {item.offeredCourse.days.map((item) => (
+              <span>{item}</span>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default MySchedule;
--- src/redux/features/student/studentCourse.api.ts
+++ src/redux/features/student/studentCourse.api.ts
@@ -29,6 +29,29 @@ const studentCourseApi = baseApi.injectEndpoints({
       },
     }),
 
+    getAllEnrolledCourses: builder.query({
+      query: (args) => {
+        const params = new URLSearchParams();
+        if (args) {
+          args.forEach((item: TQueryParam) => {
+            params.append(item.name, item.value as string);
+          });
+        }
+        return {
+          url: "/enrolled-courses/my-enrolled-courses",
+          method: "GET",
+          params: params,
+        };
+      },
+      providesTags: ["offeredCourse"],
+      transformResponse: (response: TResponseRedux<any>) => {
+        return {
+          data: response.data,
+          meta: response.meta,
+        };
+      },
+    }),
+
     enrollCourse: builder.mutation({
       query: (data) => ({
         url: "/enrolled-courses/create-enrolled-course",
@@ -40,5 +63,8 @@ const studentCourseApi = baseApi.injectEndpoints({
   }),
 });
 
-export const { useGetAllOfferedCoursesQuery, useEnrollCourseMutation } =
-  studentCourseApi;
+export const {
+  useGetAllOfferedCoursesQuery,
+  useEnrollCourseMutation,
+  useGetAllEnrolledCoursesQuery,
+} = studentCourseApi;
